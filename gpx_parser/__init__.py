6ee0416c7e286aea90b90aa19a497a1f96a56bea
olga <olga.kholkovskaia@gmail.com>
2018-07-26 14:59:05 +0200

    module names added

--- gpx_parser/__init__.py
+++ gpx_parser/__init__.py
@@ -1,12 +1,12 @@
 name='gpx_lite'
-
 from gpx_parser.GPX import GPX
+
+__all__ = ['GPX', 'GPXTrack', 'GPXTrackSegment', 'GPXTrackPoint']
 
 
 def parse(xml_or_file)->GPX:
     """
     Wrapper fo GPXParser.parse() method.
-
 
     :param xml_or_file: xml string or file handler
     :return: gpx loaded from xml
